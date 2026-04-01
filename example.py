982130ea1aee8b35da0c1d255394e8cc96b0c904
Hhung Zhaohong <huaug9672@gmail.co>
2026-04-01 17:37:10 +0800

    第二次提交 ( 20260404 )

--- example.py
+++
@@ -1,9 +0,0 @@
-sum = 0
-
-counter = 1
-while counter <= 3:
-  sum = sum + counter
-
-counter = counter + 1
-
-print('sum = ' + str(sum))
--- main.py
+++ main.py
@@ -3,24 +3,31 @@ print(" 請輸入生日資訊，程式將輸出對應星座與特質 ")
 print("\n -------- Input Block -------- ")
 My_Birthday = input("請輸入生日 YYYYMMDD : ")
 
+
 print("\n ------- Output Block --------- ")
 My_Year = My_Birthday[:4]
 My_Month = My_Birthday[4:6]
 My_Day = My_Birthday[6:]
 
 Year = int(My_Birthday[:4])  # 為了判斷平年閏年
+if Year % 400 == 0:
+    print(Year,"年為閏年")
+elif Year % 100 == 0:
+    print(Year,"年為平年")
+elif Year % 4 == 0:
+    print(Year,"年為閏年")
+else :
+    print(Year,"年為平年")
 
-
-# 1. 月份日期是否出入正確
 if (My_Month >= "01" and My_Month <= "12") and (My_Day >= "01" and My_Day <= "31"):
-    # 2. 星座判斷
     if (My_Month == "01" and My_Day >= "20") or (My_Month == "02" and My_Day <= "18"):
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("水瓶座")
+        print("星座特質:", "")
     elif (My_Month == "02" and My_Day >= "19") or (My_Month == "03" and My_Day <= "20") :
         # 如果是 2 月，需根據是否為閏年來檢查日期是否超過 28 或 29 天
         if My_Month == "02":
-            # 整合 year.py 的閏年判斷邏輯
+            # 閏年判斷邏輯
             is_leap = False
             if Year % 400 == 0:
                 is_leap = True
@@ -46,32 +53,42 @@ if (My_Month >= "01" and My_Month <= "12") and (My_Day >= "01" and My_Day <= "31
     elif (My_Month == "03" and My_Day >= "21") or (My_Month == "04" and My_Day <= "19") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 牡羊座")
+        print("星座特質:", "十二星座的領頭羊，喜歡爭第一，這個精力充沛的星座對競爭並不陌生，大膽而有企圖心，牡羊會在最艱難的狀況下無畏前行，並且一路領先。")
     elif (My_Month == "04" and My_Day >= "20") or (My_Month == "05" and My_Day <= "20") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 金牛座")
-    elif (My_Month == "05" and My_Day >= "21") or (My_Month == "06" and My_Day <= "21") :
+        print("星座特質:", "")
+    elif (My_Month == "05" and My_Day >= "21") or (My_Month == "06" and My_Day <= "20") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 雙子座")
-    elif (My_Month == "06" and My_Day >= "22") or (My_Month == "07" and My_Day <= "22") :
+        print("星座特質:", "")
+    elif (My_Month == "06" and My_Day >= "21") or (My_Month == "07" and My_Day <= "22") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 巨蟹座")
+        print("星座特質:", "")
     elif (My_Month == "07" and My_Day >= "23") or (My_Month == "08" and My_Day <= "22") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 獅子座")
+        print("星座特質:", "")
     elif (My_Month == "08" and My_Day >= "23") or (My_Month == "09" and My_Day <= "22") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 處女座")
-    elif (My_Month == "09" and My_Day >= "23") or (My_Month == "10" and My_Day <= "23") :
+        print("星座特質:", "")
+    elif (My_Month == "09" and My_Day >= "23") or (My_Month == "10" and My_Day <= "22") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 天秤座")
-    elif (My_Month == "10" and My_Day >= "24") or (My_Month == "11" and My_Day <= "22") :
+        print("星座特質:", "")
+    elif (My_Month == "10" and My_Day >= "23") or (My_Month == "11" and My_Day <= "21") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 天蠍座")
-    elif (My_Month == "11" and My_Day >= "23") or (My_Month == "12" and My_Day <= "21") :
+        print("星座特質:", "")
+    elif (My_Month == "11" and My_Day >= "22") or (My_Month == "12" and My_Day <= "21") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 射手座")
+        print("星座特質:", "")
     elif (My_Month == "12" and My_Day >= "22") or (My_Month == "01" and My_Day <= "19") :
         print("出生日期為:", My_Year ,"年", My_Month,"月", My_Day,"日" )
         print("星座: 摩羯座")
+        print("星座特質:", "")
 else :
     print("輸入錯誤")
