b89c8cacf1e1b868b7233f69964f140ed7229818
Hhung Zhaohong <huaug9672@gmail.co>
2026-04-22 13:17:53 +0800

    新增平閏年、星座、生命靈數及生肖查詢功能，並優化輸入驗證邏輯

---
+++ new/example.py
@@ -0,0 +1,8 @@
+# import random
+
+# dice = random.randint(1,6)
+# print(dice)
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+print(list)
---
+++ new/main.py
@@ -0,0 +1,5 @@
+print("歡行來到臺東大學大富翁遊戲！")
+
+Name = input("請輸入使用者名稱 : ")
+print(Name)
+
